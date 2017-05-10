a29fc33ef0c2a0e78fb24e9da5573dcad9c49623
Ulrik Sandberg <ulrik.sandberg@gmail.com>
2017-05-10 13:58:01 +0200

    Upgraded to latest amazonica

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [amazonica "0.3.95"
+                 [amazonica "0.3.98"
                   :exclusions [com.amazonaws/aws-java-sdk
                                com.amazonaws/amazon-kinesis-client]]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.124"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.127"]
                  [commons-logging "1.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [environ "1.1.0"]]
